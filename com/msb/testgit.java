61f0961ae98f77992f0c692e5f45b3100ffef2be
zhangxj <1004070547@qq.com>
2023-11-09 14:12:06 +0800

    第一次数据提交

---
+++ com/msb/testgit.java
@@ -0,0 +1,14 @@
+package com.msb;
+
+/**
+ * @author : zhangxj
+ * @date: 2023/11/9 0009  14:08
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class testgit {
+    //这是一个main方法,是程序的入口
+    public static void main(String[] args) {
+
+    }
+}
