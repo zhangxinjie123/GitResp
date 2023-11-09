2e38c5ae80027cdf2daeb3833851debfa191721d
zhangxj <1004070547@qq.com>
2023-11-09 14:52:03 +0800

    加name

--- com/msb/testgit.java
+++ com/msb/testgit.java
@@ -1,4 +1,4 @@
-package com.msb;
+ package com.msb;
 
 /**
  * @author : zhangxj
@@ -11,5 +11,6 @@ public class testgit {
     public static void main(String[] args) {
      int age;
         int add;
+        string name;
     }
 }
